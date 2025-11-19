979e7432916c40d744fc1b5a362db360741a7de5
Aliyah Miranti Daleya <254107060060@student.polinema.ac.id>
2025-11-19 10:20:54 +0700

    Modifikasi Percobaan 1

--- Bioskop05.java
+++ Bioskop05.java
@@ -1,18 +1,47 @@
-public class Bioskop05 {
+public class Bioskop {
     public static void main(String[] args) {
-        String[][] penonton = new String[4][2];
 
-        penonton[0][0] = "Amin";
-        penonton[0][1] = "Bena";
-        penonton[1][0] = "Candra";
-        penonton[1][1] = "Dela";
-        penonton[2][0] = "Eka";
-        penonton[2][1] = "Farhan";
-        penonton[3][0] = "Gisel";
+        String[][] penonton = {
+                {"Adit", "Budi"},
+                {"Citra", "Dewi"},
+                {"Eko", "Fani"},
+                {"Gilang", "Hana"}
+        };
 
-        System.out.printf("%s\t%s\n", penonton[0][0], penonton[0][1]);
-        System.out.printf("%s\t%s\n", penonton[1][0], penonton[1][1]);
-        System.out.printf("%s\t%s\n", penonton[2][0], penonton[2][1]);
-        System.out.printf("%s\t%s\n", penonton[3][0], penonton[3][1]);
+        System.out.println("=== Daftar Penonton ===");
+        for (String[] row : penonton) {
+            System.out.println(String.join(" | ", row));
+        }
+
+        System.out.println("\n=== Panjang Setiap Baris (For Loop) ===");
+        for (int i = 0; i < penonton.length; i++) {
+            System.out.println("Baris " + i + ": " + penonton[i].length);
+        }
+
+        System.out.println("\n=== Panjang Setiap Baris (Foreach Loop) ===");
+        int index = 0;
+        for (String[] row : penonton) {
+            System.out.println("Baris " + index + ": " + row.length);
+            index++;
+        }
+
+        System.out.println("\n=== Penonton Baris ke-3 (For Loop) ===");
+        for (int i = 0; i < penonton[2].length; i++) {
+            System.out.print(penonton[2][i] + " ");
+        }
+        System.out.println();
+
+        System.out.println("\n=== Penonton Baris ke-3 (Foreach Loop) ===");
+        for (String nama : penonton[2]) {
+            System.out.print(nama + " ");
+        }
+        System.out.println();
+
+        System.out.println("\n=== Semua Baris Penonton ===");
+        int baris = 0;
+        for (String[] row : penonton) {
+            System.out.println("Baris " + baris + ": " + String.join(", ", row));
+            baris++;
+        }
     }
 }
