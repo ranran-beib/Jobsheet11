73fb16e624a749b153df60d6b08b1ab7028857e7
Aliyah Miranti Daleya <254107060060@student.polinema.ac.id>
2025-11-19 09:18:52 +0700

    Percobaan3

---
+++ Numbers05.java
@@ -0,0 +1,27 @@
+public class Numbers05 {
+    public static void main(String[] args) {
+
+        int[][] num = new int[3][];
+
+        num[0] = new int[5];
+        num[1] = new int[3];
+        num[2] = new int[1];
+
+        num[0][0] = 10;
+        num[0][1] = 20;
+        num[0][2] = 30;
+        num[0][3] = 40;
+        num[0][4] = 50;
+        num[1][0] = 5;
+        num[1][1] = 15;
+        num[1][2] = 25;
+        num[2][0] = 99;
+
+        for (int i = 0; i < num.length; i++) {
+            for (int j = 0; j < num[i].length; j++) {
+                System.out.print(num[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
